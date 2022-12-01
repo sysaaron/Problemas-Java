818ab2920ae8678b03380b6eaca083417af591b4
Aaron Zuñiga <aaronzunigacdri@outlook.com>
2022-12-01 17:48:29 -0500

    Commit inicial

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el primer número");
+        int n1 = sc.nextInt();
+        System.out.println("Ingrese el segundo número");
+        int n2 = sc.nextInt();
+        
+        int[] arr1 = new int[100];
+        int[] arr2 = new int[100];
+     }
+}
